e9fa3f1a39757dd3c03a23750dbe06925d3e5bb7
LFB3 <151401002+LFB3@users.noreply.github.com>
2025-05-13 13:33:05 +0200

    Fuck it

--- calculator.h
+++
@@ -1,9 +0,0 @@
-#ifndef CALCULATOR_H
-#define CALCULATOR_H
-
-#include <vector>
-#include <string>
-
-std::string calculator(std::vector<std::string> userInput);
-
-#endif
--- main.cpp
+++ main.cpp
@@ -1,72 +1,26 @@
 #include <iostream>
+#include <sstream>
 #include <string>
 #include <vector>
-#include <algorithm>
-#include <sstream>
 
-#include "close.h"
-#include "calculator.h"
-
-using namespace std;
-
+std::string iostream() {
+    std::string input = "";
+    
+    std::cout << "run: ";
+    std::getline(std::cin, input);
+    return input;
+}
 
 int main() {
-    // Variables
-    std::string output = "0";
-    std::vector<std::string> userLogs;
-    std::vector<std::string> logs;
-
-    std::string userInput = "0";
-
-    std::vector<std::string> userInputsplit;
     while (true) {
-        // User Input
-        std::cout << "run:";
-        std::getline(std::cin, userInput);
-        userLogs.push_back(userInput);
-        transform(userInput.begin(), userInput.end(), userInput.begin(), ::tolower);
-
-        // Splitting the user input
-        std::string s = "";
-        for (int i = 0; i < userInput.size(); i++) {
-            if (userInput[i] == ' ') {
-                userInputsplit.push_back(s);
-                s.clear();
-                continue;
-            }
-            s += userInput[i];
+        std::string input = iostream();
+        std::istringstream iss(input);
+        std::string commmand;
+        if (iss >> commmand) {
         }
-        userInputsplit.push_back(s);
-
-
-        // Commands
-        if (userInput == "exit") {
-            close;
+        if (commmand == "exit" || commmand == "quit") {
+            std::cout << "Exiting the program, Goodbye." << std::endl;
+            break;
         }
-        if (userInputsplit[0] == "calc"|| userInputsplit[0] == "calculator") {
-            output = calculator(userInputsplit);
-        }
-
-
-
-        try {
-        // Versuche, die Eingabe in eine Zahl zu konvertieren
-            std::stoi(userInputsplit[0]); 
-            output = calculator(userInputsplit);
-        } 
-        catch (const std::invalid_argument& e) {
-            // Die Eingabe war keine Zahl
-            logs.push_back("Invalid Argument, the input was not a number");
-        } 
-        catch (const std::out_of_range& e) {
-            // Die Zahl ist zu groß oder zu klein für einen int
-            logs.push_back("Invalid Argument, Out of Range, the number was too big or too small for an int");
-        }
-        
-
-        // End Output
-        cout << output << endl;
-        userInputsplit.clear();
-        userInput.clear();
     }
 }
